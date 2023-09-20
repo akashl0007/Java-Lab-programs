c927667590a62012d8ded7c0e4c35f09afdc3690
Akash <akashlgowda203@gmail.com>
2023-09-20 20:31:01 +0530

    Swing Demonstration

---
+++ Swing/src/DemoSwing.java
@@ -0,0 +1,52 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class DemoSwing {
+    public static void main(String[] args) {
+        Abc obj = new Abc();
+    }
+}
+
+class Abc extends JFrame implements ActionListener {
+    JLabel l1;
+    JTextField t1;
+    JLabel l2;
+    JTextField t2;
+    JLabel l3;
+    JButton b;
+    JTextField t3;
+
+    public Abc() {
+        setLayout(new FlowLayout());
+
+        l1 = new JLabel("First Number:");
+        t1 = new JTextField(20);
+        l2 = new JLabel("Second Number:");
+        t2 = new JTextField(20);
+        b = new JButton("ADD");
+        l3 = new JLabel("Result");
+        t3 = new JTextField(20);
+
+        add(l1);
+        add(t1);
+        add(l2);
+        add(t2);
+        add(l3);
+        add(t3);
+        add(b);
+
+        b.addActionListener(this);
+        setVisible(true);
+        setSize(250, 400);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public void actionPerformed(ActionEvent ae) {
+        int num1 = Integer.parseInt(t1.getText());
+        int num2 = Integer.parseInt(t2.getText());
+        int value = num1 + num2;
+
+        t3.setText(Integer.toString(value));
+    }
+}
