28c82a1bd08909668615f90ba177596d4b59c3b1
Akash <akashlgowda203@gmail.com>
2023-09-20 14:28:46 +0530

    2nd Commit

--- Socket Programming/src/CircleClient.java
+++ Socket Programming/src/CircleClient.java
@@ -1,27 +1,24 @@
+import java.net.*;
 import java.io.*;
-import java.net.Socket;
 
 public class CircleClient {
-    public static void main (String[] args) throws Exception{
+    public static void main(String args[]) throws Exception {
         Socket sock = new Socket("127.0.0.1", 4000);
-        System.out.println("Enter the Radius: ");
+        System.out.print("Enter the radius : ");
         BufferedReader keyRead = new BufferedReader(new InputStreamReader(System.in));
         String radius = keyRead.readLine();
-
         OutputStream ostream = sock.getOutputStream();
         PrintWriter pwrite = new PrintWriter(ostream, true);
-        pwrite.print(radius);
-
+        pwrite.println(radius);
         InputStream istream = sock.getInputStream();
-        BufferedReader sockRead = new BufferedReader(new InputStreamReader(istream));
-        String result = sockRead.readLine();
+        BufferedReader socketRead = new BufferedReader(new InputStreamReader(istream));
+        String str;
+        str = socketRead.readLine();
+        System.out.println(str);
 
-        System.out.println(result);
-
+        pwrite.close();
+        socketRead.close();
         keyRead.close();
         sock.close();
-        pwrite.close();
-        sockRead.close();
-
     }
 }
